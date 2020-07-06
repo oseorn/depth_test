6d340c92ae1f640a902f9757f894166a2a8c6168
Kepler Warrington-Arroyo <k.warrington-arroyo@epfl.ch>
2020-07-06 09:29:32 +0200

    changed stuff

--- code.js
+++ code.js
@@ -3,12 +3,32 @@ var x, y, z;
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  if (!(motionP && orientationP)) noLoop();
-
 	x = 0;
   y = 0;
   z = 0;
 
+  if (typeof DeviceMotionEvent.requestPermission === 'function' &&
+      typeof DeviceOrientationEvent.requestPermission === 'function') {
+        needPermissionsRequest = true;
+        askButton = createButton('Begin');
+        askButton.size(windowWidth*6/8, windowHeight/8);
+        askButton.position(windowWidth/2 - drumButton.width/2, windowHeight/2);
+        askButton.mousePressed(() => {
+          DeviceMotionEvent.requestPermission()
+          .then(response => {
+            if (response != 'granted') {
+              permissions = false;
+            }
+          }).catch(console.error);
+
+          DeviceOrientationEvent.requestPermission()
+          .then(response => {
+            if (response != 'granted') {
+              permissions = false;
+            }
+          }).catch(console.error);
+        });
+  }
 
 }
 
